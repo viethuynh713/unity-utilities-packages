72fb37a2547e32aba3993866e498c642d7dac56a
Viet <thangnc@senspark.com>
2025-02-12 11:12:34 +0700

    Add serialize object in inspector

---
+++ Utilities/SerializedCollections/Sample/SerializedDictionarySampleTwo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AYellowpaper.SerializedCollections
+{
+    public class SerializedDictionarySampleTwo : MonoBehaviour
+    {
+        [SerializedDictionary("ID", "Person")]
+        public SerializedDictionary<int, Person> People;
+
+        [System.Serializable]
+        public class Person
+        {
+            public string FirstName;
+            public string LastName;
+
+            [Range(15, 20)]
+            public int Age;
+        }
+    }
+}
